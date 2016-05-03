b392e7157e365e05ea951cb9ed26117c1056d320
Eslem <j.eslem03@gmail.comm>
2016-05-03 03:53:55 +0200

    update-alternatives g++

--- install-dependencies.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install -y build-essential manpages-dev 
-sudo apt-get install -y libcppunit-doc libcppunit-dev gdb
-sudo apt-get install -y build-essential manpages-dev 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get -y update
-sudo apt-get install -y gcc-4.9 g++-4.9 libcurl-dev
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
